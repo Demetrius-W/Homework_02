4d35a4327296a933ce4a722ef46be79ccb3da775
Demetrius_S <demetrius.w.0813@gmail.com>
2021-12-07 00:28:44 +0300

    Добавьте файлы проекта.

---
+++ Homework_02_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                string FullName = "Селиванов Дмитрий Олегович";
+                byte Age = 25;
+                string Email = "demetrius.w.0813@gmail.com";
+                double Programing_Scores = 35.6;
+                double Math_Scores = 84.82;
+                double Phisics_Scores = 87.14;
+                string Pattern = "Ф.И.О.: {0} \nВозраст: {1} лет \nEmail: {2} \nБаллы по програмированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+                Console.WriteLine (Pattern, 
+                                   FullName, 
+                                   Age, 
+                                   Email, 
+                                   Programing_Scores, 
+                                   Math_Scores, 
+                                   Phisics_Scores);
+
+         Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_02_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                double Math_Scores = 84.82;
+                double Phisics_Scores = 87.14;
+                string Pattern = "Баллы по математике: {0}; Баллы по физике: {1}.";
+                double Sum = Math_Scores + Phisics_Scores;                  
+                double Mean = Sum/2;      
+                
+         Console.WriteLine (Pattern, Math_Scores, Phisics_Scores);         
+         Console.ReadKey();               
+
+         Console.WriteLine ($"Сумма баллов = {Sum}"); 
+         Console.ReadKey();  
+            
+         Console.WriteLine ($"Среднее арифметическое = {Mean}"); 
+         Console.ReadKey();  
+
+
+
+
+
+        }
+    }
+}
